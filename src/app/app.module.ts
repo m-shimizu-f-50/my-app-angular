899de878ba0e4528606ecadd9a2ec86032eb4d70
simizu-masato <m.shimizu@trigger4.com>
2024-04-05 16:09:30 +0900

    初めてのAngular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ItemComponent } from './item/item.component';
+import { ItemManualComponent } from './item-manual/item-manual.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ItemComponent, ItemManualComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/item-manual/item-manual.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-item-manual',
+  templateUrl: './item-manual.component.html',
+  styleUrls: ['./item-manual.component.css'],
+})
+export class ItemManualComponent {
+  title = 'item-manual-component';
+}
