be0953b2c45e3cc26dac55194f3e260539ba70b9
simizu-masato <m.shimizu@trigger4.com>
2024-04-05 16:18:59 +0900

    LessonManualコンポーネント追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ItemComponent } from './item/item.component';
 import { ItemManualComponent } from './item-manual/item-manual.component';
+import { LessonManualComponent } from './lesson-manual/lesson-manual.component';
 
 @NgModule({
-  declarations: [AppComponent, ItemComponent, ItemManualComponent],
+  declarations: [
+    AppComponent,
+    ItemComponent,
+    ItemManualComponent,
+    LessonManualComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/lesson-manual/lesson-manual.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-lesson-manual',
+  templateUrl: './lesson-manual.component.html',
+  styleUrls: ['./lesson-manual.component.css'],
+})
+export class LessonManualComponent {
+  title = 'LessonManualコンポーネントになります！';
+}
