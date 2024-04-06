e2003e24a2282a7f36dbca80a5545ef9adde229e
simizu-masato <m.shimizu@trigger4.com>
2024-04-06 20:09:08 +0900

    ライフサイクルフック

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ItemComponent } from './item/item.component';
@@ -13,7 +14,7 @@ import { LessonManualComponent } from './lesson-manual/lesson-manual.component';
     ItemManualComponent,
     LessonManualComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/item-manual/item-manual.component.ts
+++ src/app/item-manual/item-manual.component.ts
@@ -1,15 +1,40 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  OnChanges,
+  OnInit,
+} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-item-manual',
   templateUrl: './item-manual.component.html',
   styleUrls: ['./item-manual.component.css'],
 })
-export class ItemManualComponent {
+export class ItemManualComponent implements OnChanges, OnInit {
   title = 'item-manual-component';
+  temperature = 0.0;
   @Input('data') parentMessage: string = '初期値';
   @Input('test') testMessage: string = 'test';
   @Output('test-put') testClicked = new EventEmitter<string>();
   onClick(): void {
     this.testClicked.emit('子からのメッセージ');
+  }
+  ngOnChanges() {
+    console.log('ngOnChanges');
+  }
+
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    // API通信にてOpen-Meteoの天気情報を取得する
+    this.http
+      .get(
+        'https://api.open-meteo.com/v1/forecast?longitude=139.8107004&latitude=35.7100627&current_weather=true'
+      )
+      .subscribe((response: any) => {
+        // 取得後、データから気温情報を設定する
+        this.temperature = response?.current_weather?.temperature;
+      });
   }
 }
