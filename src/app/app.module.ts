bd859afa3bd11bf04e0dee36a89c9c945d0e62a3
simizu-masato <m.shimizu@trigger4.com>
2024-04-08 16:35:27 +0900

    基本的なルーティング実装方法

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
 import { ItemComponent } from './item/item.component';
 import { ItemManualComponent } from './item-manual/item-manual.component';
 import { LessonManualComponent } from './lesson-manual/lesson-manual.component';
 import { LessonIfComponent } from './lesson-if-manual/lesson-if.component';
 import { LessonTemplateForm } from './lesson-templete-form/lesson-template-form.component';
 import { LessonReactiveForm } from './lesson-reactive-form/lesson-reactive-form.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   declarations: [
@@ -20,8 +29,16 @@ import { LessonReactiveForm } from './lesson-reactive-form/lesson-reactive-form.
     LessonIfComponent,
     LessonTemplateForm,
     LessonReactiveForm,
+    DashboardComponent,
+    PageNotFoundComponent,
   ],
-  imports: [BrowserModule, HttpClientModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
